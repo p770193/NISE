603f7e139f6a2d078babf4f0694fc2b7b371a79e
p770193 <dan.d.kohler@gmail.com>
2015-04-11 03:35:34 -0500

    __init__ revisions

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 
 import lib, hamiltonians, experiments
-from .lib import *
+#from .lib import *
 __all__ = []
-__all__.extend([lib.__all__])
+__all__.extend(lib.__all__)
+__all__.extend(hamiltonians.__all__)
 
