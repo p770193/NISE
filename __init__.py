28f4263545ce562c970e39bfa668a385b42fe089
p770193 <dan.d.kohler@gmail.com>
2015-04-13 14:44:10 -0500

    Revert "__init__ revisions"
    This reverts commit 603f7e139f6a2d078babf4f0694fc2b7b371a79e.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 
 import lib, hamiltonians, experiments
-#from .lib import *
+from .lib import *
 __all__ = []
-__all__.extend(lib.__all__)
-__all__.extend(hamiltonians.__all__)
+__all__.extend([lib.__all__])
 
--- hamiltonians/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import params
-from .params import *
-from . import H0
-
-__all__ = []
-__all__.extend(params.__all__)
